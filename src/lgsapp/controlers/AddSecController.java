e1fa55d2fa5460d56ad7ce3b1e568ceb3f06fa27
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-01-17 17:09:11 +0530

    Create Add secrataries design

--- src/lgsapp/controlers/AddSecController.java
+++ src/lgsapp/controlers/AddSecController.java
@@ -1,11 +1,37 @@
 package lgsapp.controlers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddSecController implements Initializable {
+
+
+
+
+
+    double x = 0, y =0;
+    @FXML
+    void pressed(MouseEvent event) {
+        x = event.getSceneX();
+        y = event.getSceneY();
+    }
+
+    @FXML
+    void dragged(MouseEvent event) {
+
+        Node node = (Node) event.getSource();
+
+        Stage stage = (Stage) node.getScene().getWindow();
+
+        stage.setX(event.getScreenX() - x);
+        stage.setY(event.getScreenY() - y);
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
